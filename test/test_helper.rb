69540af3956b20a95895ae4898011c164985417c
Anton Smolko <asmolko@yandex.ru>
2022-03-11 22:36:42 +0300

    add setup queue_adapter to ActiveSupport::TestCase

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,6 +15,11 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
+    setup do
+      queue_adapter.perform_enqueued_jobs = true
+      queue_adapter.perform_enqueued_at_jobs = true
+    end
+
     # Add more helper methods to be used by all tests here...
     def load_fixture(filename)
       File.read(File.dirname(__FILE__) + "/fixtures/#{filename}")
