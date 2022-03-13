9ca0f3dd7bd926bf1e14c9a855808c1f9d10516a
Anton Smolko <asmolko@yandex.ru>
2022-03-13 21:42:20 +0300

    add UpdateInfoRepository test

--- test/jobs/update_info_repository_job_test.rb
+++ test/jobs/update_info_repository_job_test.rb
@@ -3,4 +3,14 @@
 require 'test_helper'
 
 class UpdateInfoRepositoryJobTest < ActiveJob::TestCase
+  setup do
+    @repository = repositories :ruby
+  end
+
+  test '#update' do
+    UpdateInfoRepositoryJob.perform_now @repository
+    assert { @repository.checks.size == 1 }
+    assert { @repository.name == 'rails-project-lvl1' }
+    assert { @repository.owner_login == 'antonsmolko' }
+  end
 end
