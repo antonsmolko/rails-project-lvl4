8274ce6e919bd574a2869f8cfb1b0779e9d80118
Anton Smolko <asmolko@yandex.ru>
2022-03-20 11:24:43 +0300

    add checks_controller_test.rb index test

--- test/controllers/api/checks_controller_test.rb
+++ test/controllers/api/checks_controller_test.rb
@@ -3,4 +3,10 @@
 require 'test_helper'
 
 class Api::ChecksControllerTest < ActionDispatch::IntegrationTest
+  test 'index' do
+    repository = repositories :one
+
+    post api_checks_path repository: { full_name: repository.full_name }
+    assert_response :success
+  end
 end
