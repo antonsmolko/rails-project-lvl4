11d5cc0e894fffe10a3043dc5cc079c0fa5e09b8
Anton Smolko <asmolko@yandex.ru>
2022-04-09 21:08:58 +0300

    fix clone_repository.rb clone path

--- app/lib/clone_repository.rb
+++ app/lib/clone_repository.rb
@@ -2,13 +2,7 @@
 
 class CloneRepository
   def self.start(repository)
-    tmp_repos_path = File.join(Dir.tmpdir, ENV['APP_NAME'])
-    FileUtils.mkdir(tmp_repos_path) unless Dir.exist? tmp_repos_path
-
-    repo_path = File.join(tmp_repos_path, repository.full_name)
-
-    FileUtils.rm_r repo_path if Dir.exist? repo_path
-    FileUtils.mkdir_p repo_path
+    repo_path = File.join('tmp/repos', repository.full_name)
 
     clone_cmd = "git clone #{repository.clone_url}"
     Open3.capture3("#{clone_cmd} #{repo_path}")
