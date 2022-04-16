9a2190157126960ca7a526d69263d3405808b818
Anton Smolko <asmolko@yandex.ru>
2022-04-16 20:41:35 +0300

    move auth routes under scope web module

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post 'auth/:provider', to: 'web/auth#request', as: :auth_request
-  get 'auth/:provider/callback', to: 'web/auth#callback', as: :callback_auth
-  delete 'auth', to: 'web/auth#destroy', as: :sign_out
-
   scope module: :web do
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+    delete 'auth', to: 'auth#destroy', as: :sign_out
+
     root 'home#index'
     get '/registration', to: 'registration#index', as: :new_user_registration
 
