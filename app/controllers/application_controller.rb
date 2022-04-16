d333c5cd04a476367100c2a852311858d62ccc4d
Anton Smolko <asmolko@yandex.ru>
2022-04-16 23:28:24 +0300

    remove default_url_options from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  class << self
-    def default_url_options
-      Rails.application.routes.default_url_options
-    end
-  end
 end
