c3cd3812b3f1c23a8ef1788be64075839137bbfc
Anton Smolko <asmolko@yandex.ru>
2022-04-16 20:18:13 +0300

    replace render status with head in Api::ChecksController::index

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -7,14 +7,14 @@ class Api::ChecksController < Api::ApplicationController
     repository = Repository.find_by!(full_name: repository_resource[:full_name])
 
     if repository.blank?
-      render status: :unprocessable_entity, json: { status: 'unprocessable_entity' }
+      head :unprocessable_entity
     end
 
     repository.update! has_webhook: true
 
     UpdateInfoRepositoryJob.perform_later repository.id
 
-    render status: :ok, json: { status: 'ok' }
+    head :ok
   end
 
   private
