9c4c0bfc1b641ec9e70bc7edce4364c2ca5ee551
Anton Smolko <asmolko@yandex.ru>
2022-04-18 13:19:34 +0300

    in OcktokitClientApiStub add BASE_URL variable, get_fixture_by_name method

--- app/lib/octokit_client_api_stub.rb
+++ app/lib/octokit_client_api_stub.rb
@@ -5,33 +5,35 @@ require 'rss'
 class OctokitClientApiStub
   attr_accessor :access_token
 
+  BASE_URL = 'https://api.github.com'
+
   def initialize(access_token)
     @access_token = access_token
   end
 
   def repos
-    body = File.read(Rails.root.join('test/fixtures/files/github_repos_response.json'))
-    url = 'https://api.github.com/user/repos'
+    body = get_fixture_by_name 'github_repos_response.json'
+    url = "#{BASE_URL}/user/repos"
 
     get_response url, body
   end
 
   def repo(github_id)
-    body = File.read(Rails.root.join('test/fixtures/files/ruby_repository_response.json'))
-    url = "https://api.github.com/repos/#{github_id}"
+    body = get_fixture_by_name 'ruby_repository_response.json'
+    url = "#{BASE_URL}/repos/#{github_id}"
 
     get_response url, body
   end
 
   def branch(full_name, branch)
-    body = File.read(Rails.root.join('test/fixtures/files/default_branch_response.json'))
-    url = "https://api.github.com/repos/#{full_name}/branches/#{branch}"
+    body = get_fixture_by_name 'default_branch_response.json'
+    url = "#{BASE_URL}/repos/#{full_name}/branches/#{branch}"
 
     get_response url, body
   end
 
   def create_hook(repo_id)
-    url = "https://api.github.com/repositories/#{repo_id}/hooks"
+    url = "#{BASE_URL}/repositories/#{repo_id}/hooks"
     body = ['success'].to_json
     config = {
       body: {
@@ -50,6 +52,10 @@ class OctokitClientApiStub
   end
 
   private
+
+  def get_fixture_by_name(name)
+    File.read(Rails.root.join('test/fixtures/files', name))
+  end
 
   def get_response(url, body = '{}', config = {})
     data = WebMock.stub_request(:get, url)
