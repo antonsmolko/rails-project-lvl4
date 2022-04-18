7d6c8a648193fc9fa3d9282d3a4c3371ad86bcc0
Anton Smolko <asmolko@yandex.ru>
2022-04-18 12:05:41 +0300

    remove has_webhook column from repositories

--- db/migrate/20220205095002_create_repositories.rb
+++ db/migrate/20220205095002_create_repositories.rb
@@ -11,7 +11,6 @@ class CreateRepositories < ActiveRecord::Migration[6.1]
       t.timestamp :pushed_at
       t.string :clone_url
       t.string :last_commit_id
-      t.boolean :has_webhook
 
       t.timestamps
 
