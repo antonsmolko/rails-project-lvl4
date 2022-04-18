36609315824722501ba8b1b053548a60f686d759
Anton Smolko <asmolko@yandex.ru>
2022-04-18 21:46:50 +0300

    implemented rm_r dir before repository clone

--- app/lib/clone_repository.rb
+++ app/lib/clone_repository.rb
@@ -2,9 +2,12 @@
 
 class CloneRepository
   def self.start(repository)
-    repo_path = File.join(Dir.tmpdir, 'repos', repository.full_name)
+    repo_path = File.join(Dir.tmpdir, 'repos', repository.full_name).to_s
 
     clone_cmd = "git clone #{repository.clone_url}"
+
+    FileUtils.rm_r repo_path if Dir.exist? repo_path
+
     Open3.capture3("#{clone_cmd} #{repo_path}")
 
     repo_path
