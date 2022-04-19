36b11df165af1f50378d157301b98b62b7df42e4
Anton Smolko <asmolko@yandex.ru>
2022-04-19 22:02:39 +0300

    fix RepositoryCheckMailer logger method

--- app/services/check_repository_service.rb
+++ app/services/check_repository_service.rb
@@ -25,7 +25,7 @@ class CheckRepositoryService
 
     RepositoryCheckMailer.with(check).check_failed.deliver_later unless passed
   rescue StandardError => e
-    Logger.error(e.message)
+    Logger.fatal "CheckRepositoryService error: #{e.message}"
     RepositoryCheckMailer.with(check: check, error: e.message).check_error.deliver_later
     check.mark_as_failed!
   end
