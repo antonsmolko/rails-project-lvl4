401b757dd9c06fe4aa3273ea43edebe3a60b1773
Anton Smolko <asmolko@yandex.ru>
2022-04-21 13:19:33 +0300

    fix linter errors

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: %w[user public_repo admin:repo_hook]
+  provider :github, ENV.fetch('GITHUB_CLIENT_ID', nil), ENV.fetch('GITHUB_CLIENT_SECRET', nil), scope: %w[user public_repo admin:repo_hook]
 end
